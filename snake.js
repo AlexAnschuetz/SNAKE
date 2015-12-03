b9af7552ead06abbe01e77123b4eb908263c1922
Alex Anschuetz <alexanschuetz@gmail.com>
2015-12-02 16:01:04 -0800

    adds new barriers every 10 seconds, existing for 5 seconds each

--- snake.js
+++ snake.js
@@ -1,54 +1,34 @@
+
 var x = 575;
 var y = 250;
-var framelength = 2;
+var framelength = 4;
 var z = 0;
 var t = 0;
 var clicked = 0;
 var keysPressed= {};
 var moveDirection = {};
 var modifier = 1
-
-
 var Canvas = document.getElementById("mycanvas");
 var ctx = Canvas.getContext("2d")
 ctx.fillStyle = "#FF0000";
 ctx.fillRect(x,y,5,5)
-
-//setInterval(
-	generateEvilBarrier()
-	//, 5000)
-
-
-
-
-var displayBarrier = function () {
+var displayBarrier = function(){
+	console.log("running displayBarrier")
 	ctx.fillStyle = "#FFD700";
 	ctx.fillRect( xONE,0, 1 ,489 )
 	ctx.fillRect(0, yONE , 1139 ,1 )
 	ctx.fillRect(xTWO,0, 1 ,489 )
 	ctx.fillRect(0, yTWO,1139 , 1 )
-
-
 }
-
 var removeBarrier = function () {
 	ctx.clearRect( xONE,0, 1 ,489 )
 	ctx.clearRect(0, yONE , 1139 ,1 )
 	ctx.clearRect(xTWO,0, 1 ,489 )
 	ctx.clearRect(0, yTWO,1139 , 1 )
 }
-
-
-
-
-
-
 function increaseDifficulty() {
-modifier += .1
+	// modifier += .1
 }
-
-
-
 function moveRight() {
 	if (moveDirection != "right") {
 		moveDirection="right";
@@ -60,7 +40,8 @@ function moveRight() {
 					ctx.fillRect( x, y,5,5); 
 					ctx.fillStyle = "#FF0000";
 	 				x +=1*modifier;
-	 				ctx.clearRect(x-(2+z),y,1*modifier,6)
+	 				ctx.clearRect(x-(2+z),y,1*modifier,6);
+	 				testEvilBarrier()
 	 				
 	 				
 					
@@ -70,7 +51,6 @@ function moveRight() {
 					x=0;
 					FoodHack()
 				
-
 				}
 				if (moveDirection != "right") {
 					clearInterval(moveTime)
@@ -79,8 +59,6 @@ function moveRight() {
 		}
 	}
 }
-
-
 function moveLeft() {
 	if (moveDirection != "left") {
 		moveDirection="left";
@@ -93,6 +71,7 @@ function moveLeft() {
 					ctx.fillStyle = "#FF0000";
 				 	x -=1*modifier;
 				 	ctx.clearRect(x+(6+z),y,6*modifier,6)
+				 	testEvilBarrier()
 				 	
 				 	
 					
@@ -110,7 +89,6 @@ function moveLeft() {
 		}
 	}
 }
-
 function moveUp() {
 	if (moveDirection != "up") {
 		moveDirection="up";
@@ -123,16 +101,15 @@ function moveUp() {
 					ctx.fillStyle = "#FF0000";
 				 	y -=1*modifier;
 				 	ctx.fillRect( x, y,5,5); 
+				 	testEvilBarrier()
 				 	
 				 	
 				}
 				else if (y= -5)
 				{
-
 					y=489;
 					FoodHack()
 					
-
 				}
 				if (moveDirection != "up") {
 					clearInterval(moveTime)
@@ -142,13 +119,11 @@ function moveUp() {
 	}
 	// moveUp()
 }
-
 function moveDown() {
 	if (moveDirection != "down") {
 		moveDirection="down";
 		if (moveDirection == "down") {
 			var moveTime = setInterval(function(){
-
 				if( y<489) {
 					eatFood()
 					eatBigFood()
@@ -156,6 +131,7 @@ function moveDown() {
 					ctx.fillStyle = "#FF0000";
 	 				y +=1*modifier;
 	 				ctx.clearRect(x,y-(5+z),6,6*modifier);
+	 				testEvilBarrier()
 	 				
 	 				
 	 				
@@ -166,7 +142,6 @@ function moveDown() {
 					y=0;
 					FoodHack()
 				
-
 				}
 				if (moveDirection != "down") {
 					clearInterval(moveTime)
@@ -174,15 +149,11 @@ function moveDown() {
 			}, framelength)
 		}
 	}
-	
 }
-
-
 function startButton () {
-var rando = Math.ceil(Math.random()*4)
-
+	var rando = Math.ceil(Math.random()*4)
 //attempting to create start button that sends square in random direction upon press
-document.getElementById("btn").addEventListener("click", function(){
+	document.getElementById("btn").addEventListener("click", function(){
     
     	if (rando === 4) {
     		moveUp()
@@ -196,88 +167,51 @@ document.getElementById("btn").addEventListener("click", function(){
     	else if (rando === 1) {
     		moveRight()
     	}
-});
- }
-
-
-
-
-
+	});
+}
 // Handle keyboard controls
-
-
-
 function clickStartMandatory() {
-document.getElementById('btn').addEventListener("click", function() {
-	clicked = 1;
-	enableArrowKeys()
-	setInterval(increaseDifficulty, 5000)
+	document.getElementById('btn').addEventListener("click", function() {
+		clicked = 1;
+		enableArrowKeys()
+		generateEvilBarrier()
+		setInterval(increaseDifficulty, 5000)
 	//above makes snake speed up as game goes on
-
 	//setInterval( qrFoodHack, 100)
 	//resets booard frequently to clear snake trail
-
+	})
 }
-)
-}
-
 function enableArrowKeys() {
-if (clicked === 1) {
-
-addEventListener("keydown", checkKeyPressed);
-
-	function checkKeyPressed(e) {
-	if (e.keyCode == "37") {
-		
-		
-		moveLeft()
-		FoodHack()
-		
-	}
-	if (e.keyCode == "38") {
-		
-		
-		moveUp()
-		FoodHack()
-	
-	}
-	if (e.keyCode == "39") {
-		
-		
-		moveRight()
-		FoodHack()
-	
-	}
-	if (e.keyCode == "40") {
-		
-	
-		moveDown()
-		FoodHack()
-		
+	if (clicked === 1) {
+		addEventListener("keydown", checkKeyPressed);
+		function checkKeyPressed(e) {
+			if (e.keyCode == "37") {
+				moveLeft()
+				FoodHack()
+			}
+			if (e.keyCode == "38") {
+				moveUp()
+				FoodHack()
+			}
+			if (e.keyCode == "39") {
+				moveRight()
+				FoodHack()
+			}
+			if (e.keyCode == "40") {
+				moveDown()
+				FoodHack()	
+			}
+		}
 	}
 }
-}
-}
-
-
-
-
  function generateFood () {
  	 q = Math.ceil(Math.random()*1137)
  	 r = Math.ceil(Math.random()*487)
-
  	ctx.fillStyle = "#000000";
 	ctx.fillRect(q,r,5,5)
-
-
- }
-
-
-
+}
  function eatFood () {
- 	
-
- 	if (     (Math.abs(x-q) < 5) && (Math.abs(y-r) < 5)  ) {
+ 	if ((Math.abs(x-q) < 5) && (Math.abs(y-r) < 5)) {
  		ctx.clearRect(q,r,5,5);
  		t+=25
  		document.getElementById('score').innerHTML = t 
@@ -286,37 +220,27 @@ addEventListener("keydown", checkKeyPressed);
  
  	}
 }
-
 function FoodHack () {
 	//if (e.keyCode == "40" || "37" || "38" || "39") {
 	ctx.clearRect(0,0,1139,489)
 	ctx.fillStyle = "#000000";
 	ctx.fillRect(q,r,5,5)
-
 	ctx.fillStyle = "#00FF00";
 	ctx.fillRect(b,n,10,10)
-
 	/*ctx.fillStyle = "#FFD700";
 	ctx.fillRect(xONE,0, 1 ,489 )
 	ctx.fillRect(0,yONE, 1139 ,1 )
 	ctx.fillRect(xTWO,0, 1 ,489 )
 	ctx.fillRect(0,yTWO,1139 , 1 ) */
 }
-
-
-
 function generateBigFood () {
 	b = Math.ceil(Math.random()*1137)
 	n = Math.ceil(Math.random()*487)
-
 	ctx.fillStyle = "#00FF00";
 	ctx.fillRect(b,n,10,10)
 }
-
-
-
 function eatBigFood () {
-	if(		(Math.abs(x-b) < 10) && (Math.abs(y-n) < 10) ) {
+	if((Math.abs(x-b) < 10) && (Math.abs(y-n) < 10) ) {
 		ctx.clearRect(b,n,10,10)
 		t+=50
 		document.getElementById('score').innerHTML = t
@@ -324,49 +248,43 @@ function eatBigFood () {
 		z+=12
 	}
 }
-
 function generateEvilBarrier() {
- if (clicked == 1) {
-	xONE = Math.ceil(Math.random()*1137)
-	yONE = Math.ceil(Math.random()*487)
-	xTWO = Math.ceil(Math.random()*1137)
-	yTWO = Math.ceil(Math.random()*487)
-
-	//everytime evil barrier is generated i want to run the following for some amount of time
-setInterval(displayBarrier, 100)
-setTimeout(clearInterval(displayBarrier), 60000)
-setTimeout(makeNull,60000)
+ 	if (clicked == 1) {
+		xONE = Math.ceil(Math.random()*1137)
+		yONE = Math.ceil(Math.random()*487)
+		xTWO = Math.ceil(Math.random()*1137)
+		yTWO = Math.ceil(Math.random()*487)
+		//everytime evil barrier is generated i want to run the following for some amount of time
+	
+	setInterval(displayBarrier, 100)
+	setTimeout(clearInterval(displayBarrier), 5000)
+	setTimeout(makeNull, 5000)
 
 	}
 }
 
+
+setInterval(generateEvilBarrier, 10000)
+// setTimeout(clearInterval(displayBarrier), 60000)
+// setTimeout(makeNull,60000)
 function makeNull () {
 	xONE = null
 	yONE = null
 	xTWO = null
 	yTWO = null
 }
-
-
 function testEvilBarrier() {
 	if ((Math.abs(x-xONE) < 10) || (Math.abs(y-yONE) < 10) ) {
 		alert("You touched the evil barrier. You Lose !")
-		
 	}
 	if ((Math.abs(x-xTWO) < 10) || (Math.abs(y-yTWO) < 10) ) {
 		alert("You touched the evil barrier. You Lose!")
-		
 	}
 }
-
-
-
 startButton()
 clickStartMandatory()
 generateFood()
 generateBigFood()
-
-
 /*
 addEventListener("keyup", function (e) {
 	keysPressed[e.keyCode] = true;
@@ -386,11 +304,6 @@ var update = function (modifier) {
 		moveRight()
 	}
 }
-*/
-
-
-
-
-
+*/ // sup
 //gives error when run but if you refresh error goes away
 // why isnt global x changes in function displayed in ctx.fillRect ??
